ff1e5eee5df576b07d9f1288df2ae40c5b24e12d
ChoccyMilkBoi <armijoemilio08@gmail.com>
2024-04-02 21:04:32 -0500

    modified:   PortfolioWebApp/index.html 	modified:   PortfolioWebApp/package-lock.json 	modified:   PortfolioWebApp/package.json 	modified:   PortfolioWebApp/src/App.tsx 	new file:   PortfolioWebApp/src/Components/HeroSection.tsx 	modified:   PortfolioWebApp/src/index.css 	modified:   PortfolioWebApp/src/main.tsx

--- PortfolioWebApp/src/App.tsx
+++ PortfolioWebApp/src/App.tsx
@@ -1,12 +1,11 @@
 import { useState } from "react";
-import "./App.css";
 
 function App() {
-	return (
-		<>
-			<div></div>
-		</>
-	);
+  return (
+    <>
+      <h1 className="text-5xl font-bold underline">Hello world!</h1>
+    </>
+  );
 }
 
 export default App;
---
+++ PortfolioWebApp/src/Components/HeroSection.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+function HeroSection() {
+  return (
+    <>
+      <div>
+        <div>
+          <h1>Hello</h1>
+          <p></p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default HeroSection;
